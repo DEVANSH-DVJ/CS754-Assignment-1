6b79c484d7634964e03b6d68485850628b47bb56
devansh-dvj <devansh.dvj@gmail.com>
2021-02-09 02:50:15 +0530

    Added OMP and it works not so well for non-overlapping patches

--- MATLAB/q2.m
+++ MATLAB/q2.m
@@ -1,9 +1,13 @@
+clc;
+clear;
+close all;
+
 addpath("MMread");
 video = mmread('../data/cars.avi');
 
-x_min = 168;
+x_min = 169;
 x_max = 288;
-y_min = 112;
+y_min = 113;
 y_max = 352;
 H = x_max - x_min + 1;
 W = y_max - y_min + 1;
@@ -11,14 +15,40 @@ T=3;
 
 F = zeros(H,W,T,'uint8');
 for i=1:T
-   F(:,:,i) = rgb2gray(video.frames(i).cdata(x_min:x_max, y_min:y_max, :));
-%    figure;
-%    imshow(F(:,:,i));
+    F(:,:,i) = rgb2gray(video.frames(i).cdata(x_min:x_max, y_min:y_max, :));
+    figure;
+    imshow(F(:,:,i));
 end
 
 C = randi([0, 1], H, W, T, 'uint8');
 
-E = sum(C.*F, 3) + 2*randn(H,W);
+E = sum(C.*F, 3)/T + 2*randn(H,W);
 figure;
-imshow(E);
+imshow(cast(E,'uint8'));
 
+D1 = dctmtx(8);
+D2 = kron(D1,D1);
+psi = kron(D2, dctmtx(T));
+
+R = zeros(H,W,T,'uint8');
+
+for i=1:H/8
+    for j=1:W/8
+        y = reshape(E(8*(i-1)+1:8*i,8*(j-1)+1:8*j), [8*8 1]);
+
+        phi = zeros(8*8*T, 8*8, 'double');
+        for k=1:T
+            phi(8*8*(k-1)+1:8*8*k,:) = diag(reshape(C(8*(i-1)+1:8*i,8*(j-1)+1:8*j,k), [8*8 1]));
+        end
+        
+        x = omp(phi.'*psi,y,40);
+        R(8*(i-1)+1:8*i,8*(j-1)+1:8*j,:) = reshape(psi*x, [8 8 T]);
+    
+%        finish;
+    end
+end
+
+for i=1:T
+    figure;
+    imshow(R(:,:,i));
+end
