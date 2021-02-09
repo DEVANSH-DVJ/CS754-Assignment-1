e5d7a27eca1de27e160eb20ba3554f7dea4a576d
devansh-dvj <devansh.dvj@gmail.com>
2021-02-09 21:38:11 +0530

    Q2 - completed all reconstructions - not sure about it

--- MATLAB/q2.m
+++ MATLAB/q2.m
@@ -3,17 +3,29 @@ clear;
 close all;
 
 addpath("MMread");
-rng(100);
+rng(20);
 
 video = mmread('../data/cars.avi');
+name = 'cars';
 x_min = 169;
 x_max = 288;
 y_min = 113;
 y_max = 352;
+T = 3;
+% T = 5;
+% T = 7;
+
+% video = mmread('../data/flame.avi');
+% name = 'flame';
+% x_min = 1;
+% x_max = 288;
+% y_min = 1;
+% y_max = 352;
+% T = 5;
+
 H = x_max - x_min + 1;
 W = y_max - y_min + 1;
 noise_std = 2;
-T = 3;
 
 F = zeros(H,W,T,'double');
 for i=1:T
@@ -25,17 +37,17 @@ end
 C = randi([0, 1], H, W, T, 'double');
 
 E = sum(C.*F, 3) + noise_std*randn(H,W);
-E = E/T; C = C/T;
+
 figure;
-imshow(cast(E,'uint8'));
-imwrite(cast(E, 'uint8'), sprintf('plots/%i_coded_snapshot.jpg',T));
+imshow(cast(E/T,'uint8'));
+imwrite(cast(E/T, 'uint8'), sprintf('plots/%s_%i_coded_snapshot.jpg',name,T));
 
 D1 = dctmtx(8);
-D2 = kron(D1,D1);
+D2 = kron(D1, D1);
 psi = kron(D2, dctmtx(T));
 
 R = zeros(H,W,T,'double');
-avg_mat = zeros(H,W,T,'double');
+avg_mat = zeros(H,W,'double');
 
 % for i=1:H/8
 %     for j=1:W/8
@@ -64,23 +76,23 @@ for i=1:H-7
             phi(8*8*(k-1)+1:8*8*k,:) = diag(reshape(C(i:i+7,j:j+7,k), [8*8 1]));
         end
         
-%         x = omp(phi.'*psi, y, 10);
-        x = omp_e(phi.'*psi, y, 9*8*8*noise_std^2);
+%         x = omp(phi.'*psi, y, 20);
+        x = omp_e(phi'*psi, y, 9*8*8*noise_std^2);
         R(i:i+7,j:j+7,:) = R(i:i+7,j:j+7,:) + reshape(psi*x, [8 8 T]);
-        avg_mat(i:i+7,j:j+7,:) = avg_mat(i:i+7,j:j+7,:) + ones(8,8,T);
+        avg_mat(i:i+7,j:j+7) = avg_mat(i:i+7,j:j+7) + ones(8,8);
         i, j
     end
 end
 
 for i=1:T
+    R(:,:,i) = R(:,:,i)./avg_mat(:,:);
     figure;
 %     imshow(cast(R(:,:,i)./avg_mat(:,:,i), 'uint8'));
-    imshow(cast([R(:,:,i)./avg_mat(:,:,i), F(:,:,i)], 'uint8'));
-    imwrite(cast([R(:,:,i)./avg_mat(:,:,i), F(:,:,i)], 'uint8'), sprintf('plots/%i_%i.png',T,i));
+    imshow(cast([R(:,:,i), F(:,:,i)], 'uint8'));
+    imwrite(cast([R(:,:,i), F(:,:,i)], 'uint8'), sprintf('plots/%s_%i_%i.png',name,T,i));
+    fprintf('RMSE for frame %i : %f\n',i,norm(R(:,:,i)-F(:,:,i), 'fro')^2/norm(F(:,:,i), 'fro')^2);
 end
 
-% l = R(:,:,1)./avg_mat(:,:,1);
-% norm(l-F(:,:,1), 'fro')/norm(F(:,:,1), 'fro');
 toc;
 
 function theta = omp_e(A, y, e)
@@ -92,7 +104,7 @@ function theta = omp_e(A, y, e)
     i = 0;                   % iteration
     A_omega = [];
 
-    while(i < 64 && norm(r)^2 > e)
+    while(i < N && norm(r)^2 > e)
         i = i + 1;
         x_tmp = zeros(K,1);
         indices = setdiff(1:K, T); % iterate all columns except for the chosen ones
